e99b22273cf22805113ab1022911c5e07795a2a1
pouria.pirzadeh@gmail.com <pouria.pirzadeh@gmail.com>
2016-02-17 18:01:14 -0800

    Modifying install script to handle output directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,10 +9,13 @@ then
   exit 1
 fi 
 
+mkdir -p $SOCIAL_GEN_HOME/output
+
 for i in `cat $SOCIAL_GEN_HOME/conf/machines`
 do
     ssh $i mkdir -p $SOCIAL_GEN_LOCAL_HOME
     ssh $i mkdir -p $SOCIAL_GEN_LOCAL_HOME/target
+    ssh $i mkdir -p $SOCIAL_GEN_LOCAL_HOME/output	
     scp -q $SOCIAL_GEN_HOME/target/SocialGen.jar $i:$SOCIAL_GEN_LOCAL_HOME/target
-    scp -q -r $SOCIAL_GEN_HOME/conf $SOCIAL_GEN_HOME/metadata $SOCIAL_GEN_HOME/output $SOCIAL_GEN_HOME/scripts $i:$SOCIAL_GEN_LOCAL_HOME &
+    scp -q -r $SOCIAL_GEN_HOME/conf $SOCIAL_GEN_HOME/metadata $SOCIAL_GEN_HOME/scripts $i:$SOCIAL_GEN_LOCAL_HOME &
 done
