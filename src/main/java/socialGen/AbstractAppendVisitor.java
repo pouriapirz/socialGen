ea1068445d98576737d62cb5e72e0e11f5cd0612
Till Westmann <tillw@apache.org>
2016-02-25 00:06:30 -0800

    factor out common code in ADM/JSON visitor

--- src/main/java/socialGen/AbstractAppendVisitor.java
+++ src/main/java/socialGen/AbstractAppendVisitor.java
@@ -1,5 +1,14 @@
 package socialGen;
 
+import java.util.List;
+
+import datatype.Message;
+import entity.Employments;
+import entity.FacebookMessage;
+import entity.FacebookUser;
+import entity.TweetMessage;
+import entity.TwitterUser;
+
 public abstract class AbstractAppendVisitor implements IAppendVisitor {
     protected final StringBuilder builder = new StringBuilder();
 
@@ -12,8 +21,90 @@ public abstract class AbstractAppendVisitor implements IAppendVisitor {
         return builder.toString();
     }
 
+    abstract String startList();
+
+    abstract String endList();
+
+    abstract String startBag();
+
+    abstract String endBag();
+
     public IAppendVisitor append(String s) {
         builder.append(s);
         return this;
+    }
+
+    public IAppendVisitor visit(String s) {
+        builder.append('"').append(s).append('"');
+        return this;
+    }
+
+    public IAppendVisitor visit(List<String> strs) {
+        builder.append(startBag());
+        final int size = strs.size();
+        for (int i = 0; i < size - 1; i++) {
+            visit(strs.get(i));
+            builder.append(", ");
+        }
+        if (size > 0) {
+            visit(strs.get(size - 1));
+        }
+        builder.append(endBag());
+        return this;
+    }
+
+    public IAppendVisitor visit(int i) {
+        builder.append(i);
+        return this;
+    }
+
+    public IAppendVisitor visit(long[] l) {
+        builder.append(startBag());
+        for (int i = 0; i < l.length - 1; i++) {
+            visit(l[i]);
+            builder.append(", ");
+        }
+        if (l.length > 0) {
+            visit(l[l.length - 1]);
+        }
+        builder.append(endBag());
+        return this;
+    }
+
+    public IAppendVisitor visit(Message message) {
+        builder.append('"');
+        for (int i = 0; i < message.getLength(); i++) {
+            builder.append(message.charAt(i));
+        }
+        builder.append('"');
+        return this;
+    }
+
+    public IAppendVisitor visit(Employments employment) {
+        builder.append(startList());
+        int empCount = employment.size();
+        for (int i = 0; i < empCount - 1; i++) {
+            employment.get(i).accept(this);
+            builder.append(", ");
+        }
+        employment.get(empCount - 1).accept(this);
+        builder.append(endList());
+        return this;
+    }
+
+    public IAppendVisitor visit(FacebookMessage fbMessage) {
+        return fbMessage.accept(this);
+    }
+
+    public IAppendVisitor visit(FacebookUser fbUser) {
+        return fbUser.accept(this);
+    }
+
+    public IAppendVisitor visit(TweetMessage twMessage) {
+        return twMessage.accept(this);
+    }
+
+    public IAppendVisitor visit(TwitterUser twUser) {
+        return twUser.accept(this);
     }
 }
